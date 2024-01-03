2ce7b28c8947b57cd7245154468efd0b3070c015
iadam <iadam@ADAM>
2024-01-02 19:46:39 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Adam and Jake!");
+            Console.WriteLine("Popeye's Chicken Sandwish");
+
+        }
+
+
+
+    } // class
+
+} // namespace
+
